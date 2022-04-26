0754dd81c9cc5197a0ca4c4e5797f5db53475ab7
Eli Jones <biblicabeebli@gmail.com>
2022-04-26 17:00:09 -0400

    [ios-upload-fixes] creates reference csv header constants.

--- constants/data_processing_constants.py
+++ constants/data_processing_constants.py
@@ -1,5 +1,163 @@
 ## Chunks
 # This value is in seconds, it sets the time period that chunked files will be sliced into.
+from constants.data_stream_constants import (ACCELEROMETER, ANDROID_LOG_FILE, BLUETOOTH, CALL_LOG, DEVICE_IDENTIFIERS_HEADER,
+    DEVICEMOTION, GPS, GYRO, IDENTIFIERS, IOS_LOG_FILE, MAGNETOMETER, POWER_STATE, PROXIMITY,
+    REACHABILITY, SURVEY_ANSWERS, SURVEY_TIMINGS, TEXTS_LOG, WIFI)
+from constants.participant_constants import ANDROID_API, IOS_API
+
+
 CHUNK_TIMESLICE_QUANTUM = 3600
+
 # the name of the s3 folder that contains chunked data
 CHUNKS_FOLDER = "CHUNKED_DATA"
+
+# These reference dicts contain the output headers that should exist for each data stream, per-os.
+#  A value of None means that the os cannot generate that data (or the dictionary needs to be updated)
+
+REFERENCE_CHUNKREGISTRY_HEADERS = {
+    ACCELEROMETER: {
+        ANDROID_API: b'timestamp,UTC time,accuracy,x,y,z',
+        IOS_API:     b'timestamp,UTC time,accuracy,x,y,z',
+    },
+    ANDROID_LOG_FILE: {
+        ANDROID_API: b'timestamp,UTC time,event',
+        IOS_API:     b'timestamp,UTC time,event',
+    },
+    BLUETOOTH: {
+        ANDROID_API: b'timestamp,UTC time,hashed MAC,RSSI',
+        IOS_API: None
+    },
+    CALL_LOG: {
+        ANDROID_API: b'timestamp,UTC time,hashed phone number,call type,duration in seconds',
+        IOS_API: None
+    },
+    DEVICEMOTION: {
+        ANDROID_API: None,
+        IOS_API:     b'timestamp,UTC time,roll,pitch,yaw,rotation_rate_x,rotation_rate_y,rotation_rate_z,gravity_x,gravity_y,gravity_z,user_accel_x,user_accel_y,user_accel_z,magnetic_field_calibration_accuracy,magnetic_field_x,magnetic_field_y,magnetic_field_z',
+    },
+    GPS: {
+        ANDROID_API: b'timestamp,UTC time,latitude,longitude,altitude,accuracy',
+        IOS_API:     b'timestamp,UTC time,latitude,longitude,altitude,accuracy',
+    },
+    GYRO: {
+        ANDROID_API: b'timestamp,UTC time,accuracy,x,y,z',
+        IOS_API:     b'timestamp,UTC time,x,y,z',
+    },
+    IDENTIFIERS: {
+        ANDROID_API: b'timestamp,UTC time,patient_id,MAC,phone_number,device_id,device_os,os_version,product,brand,hardware_id,manufacturer,model,beiwe_version',
+        IOS_API:     b'timestamp,UTC time,patient_id,MAC,phone_number,device_id,device_os,os_version,product,brand,hardware_id,manufacturer,model,beiwe_version',
+    },
+    # IMAGE_FILE: {  # this is an image survey file, which appears (from our development staging server so this is not the final word to have multiple possible) headers.  Image surveys aren't actually a thing, so comment out for now.
+    #     ANDROID_API: None,
+    #     IOS_API:     b'index,question1_answer,question2_answer,created_on_timestamp,gps_info',
+    #               'question1_answer,question2_answer,created_on_timestamp,gps_info',
+    # },
+    IOS_LOG_FILE: {
+        ANDROID_API: None,
+        IOS_API:     b'timestamp,UTC time,launchId,memory,battery,event,msg,d1,d2,d3,d4',
+    },
+    MAGNETOMETER: {
+        ANDROID_API: None,
+        IOS_API:     b'timestamp,UTC time,x,y,z',
+    },
+    POWER_STATE: {
+        ANDROID_API: b'timestamp,UTC time,event',
+        IOS_API:     b'timestamp,UTC time,event,level',
+    },
+    PROXIMITY: {
+        ANDROID_API: None,
+        IOS_API:     b'timestamp,UTC time,event',
+    },
+    REACHABILITY: {
+        ANDROID_API: None,
+        IOS_API:     b'timestamp,UTC time,event',
+    },
+    # SURVEY_ANSWERS: {  # we don't chunk survey answers...
+    #     ANDROID_API: b'question id,question type,question text,question answer options,answer',
+    #     IOS_API:     b'question id,question type,question text,question answer options,answer',
+    # },
+    SURVEY_TIMINGS: {
+        ANDROID_API: b'timestamp,UTC time,question id,survey id,question type,question text,question answer options,answer',
+        IOS_API:     b'timestamp,UTC time,question id,survey id,question type,question text,question answer options,answer,event',
+    },
+    TEXTS_LOG: {
+        ANDROID_API: b'timestamp,UTC time,hashed phone number,sent vs received,message length,time sent',
+        IOS_API: None
+    },
+    WIFI: {
+        ANDROID_API: b'timestamp,UTC time,hashed MAC,frequency,RSSI',
+        IOS_API:     b'timestamp,UTC time,hashed MAC,frequency,RSSI',
+    }
+}
+
+
+REFERENCE_UPLOAD_HEADERS = {
+    ACCELEROMETER: {
+        ANDROID_API: b'timestamp,accuracy,x,y,z',
+        IOS_API:     b'timestamp,accuracy,x,y,z',
+    },
+    ANDROID_LOG_FILE: {
+        ANDROID_API: b'THIS LINE IS A LOG FILE HEADER',
+        IOS_API: None
+    },
+    BLUETOOTH: {
+        ANDROID_API: b'timestamp, hashed MAC, RSSI',
+        IOS_API: None
+    },
+    CALL_LOG: {
+        ANDROID_API: b'hashed phone number,call type,timestamp,duration in seconds',
+        IOS_API: None
+    },
+    DEVICEMOTION: {
+        ANDROID_API: None,
+        IOS_API:     b'timestamp,roll,pitch,yaw,rotation_rate_x,rotation_rate_y,rotation_rate_z,gravity_x,gravity_y,gravity_z,user_accel_x,user_accel_y,user_accel_z,magnetic_field_calibration_accuracy,magnetic_field_x,magnetic_field_y,magnetic_field_z',
+    },
+    GPS: {
+        ANDROID_API: b'timestamp, latitude, longitude, altitude, accuracy',
+        IOS_API:     b'timestamp,latitude,longitude,altitude,accuracy',
+    },
+    GYRO: {
+        ANDROID_API: b'timestamp,accuracy,x,y,z',
+        IOS_API:     b'timestamp,x,y,z',
+    },
+    IDENTIFIERS: {
+        ANDROID_API: DEVICE_IDENTIFIERS_HEADER,
+        IOS_API: DEVICE_IDENTIFIERS_HEADER,
+    },
+    IOS_LOG_FILE: {
+        ANDROID_API: None,
+        IOS_API:     b'timestamp,launchId,memory,battery,event,msg,d1,d2,d3,d4',
+    },
+    MAGNETOMETER: {
+        ANDROID_API: None,
+        IOS_API:     b'timestamp,x,y,z',
+    },
+    POWER_STATE: {
+        ANDROID_API: b'timestamp, event',
+        IOS_API:     b'timestamp,event,level',
+    },
+    PROXIMITY: {
+        ANDROID_API: None,
+        IOS_API:     b'timestamp,event',
+    },
+    REACHABILITY: {
+        ANDROID_API: None,
+        IOS_API:     b'timestamp,event',
+    },
+    # SURVEY_ANSWERS: {  # we don't chunk survey answers...
+    #     ANDROID_API: b'question id,question type,question text,question answer options,answer',
+    #     IOS_API:     b'question id,question type,question text,question answer options,answer',
+    # },
+    SURVEY_TIMINGS: {
+        ANDROID_API: b'timestamp,question id,question type,question text,question answer options,answer',
+        IOS_API:     b'timestamp,question id,question type,question text,question answer options,answer,event',
+    },
+    TEXTS_LOG: {
+        ANDROID_API: b'timestamp,hashed phone number,sent vs received,message length,time sent',
+        IOS_API: None,
+    },
+    WIFI: {
+        ANDROID_API: b'hashed MAC, frequency, RSSI',
+        IOS_API: None
+    }
+}

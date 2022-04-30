81d3166baa39a0930e08dbd0167242396d05db77
Eli Jones <biblicabeebli@gmail.com>
2022-04-30 17:28:12 -0400

    [staged-updates] cleanups of s3

--- libs/s3.py
+++ libs/s3.py
@@ -1,6 +1,7 @@
-from typing import Generator, List, Union
+from typing import List
+
 import boto3
-from botocore.client import BaseClient
+from botocore.client import BaseClient, Paginator
 from Cryptodome.PublicKey import RSA
 
 from config.settings import (BEIWE_SERVER_AWS_ACCESS_KEY_ID, BEIWE_SERVER_AWS_SECRET_ACCESS_KEY,
@@ -12,20 +13,8 @@ from libs.internal_types import StrOrParticipantOrStudy
 from libs.rsa import generate_key_pairing, get_RSA_cipher, prepare_X509_key_for_java
 
 
-"""
-Research on getting a stream into the decryption code of pycryptodome
-
-The StreamingBody StreamingBody object does not define the __len__ function, which is
-necessary for creating a buffer somewhere in the decryption code, but it is possible to monkeypatch
-it in like this:
-    import botocore.response
-    def monkeypatch_len(self):
-        return int(self._content_length)
-    botocore.response.StreamingBody.__len__ = monkeypatch_len
-
-But that just results in this error from pycryptodome:
-TypeError: Object type <class 'botocore.response.StreamingBody'> cannot be passed to C code
-"""
+# NOTE: S3_BUCKET is patched during tests to be the Exception class, which is (obviously) invalid.
+# The asserts in this file are protections for runninsg s3 commands inside tests.
 
 
 class S3VersionException(Exception): pass
@@ -40,7 +29,6 @@ conn: BaseClient = boto3.client(
 )
 
 
-# NOTE: the S3_BUCKET variable is patched during tests to be the Exception class
 def smart_get_study_encryption_key(obj: StrOrParticipantOrStudy) -> bytes:
     if isinstance(obj, Participant):
         return obj.study.encryption_key.encode()
@@ -62,10 +50,6 @@ def s3_construct_study_key_path(key_path: str, obj: StrOrParticipantOrStudy):
     else:
         raise TypeError(f"expected Study, Participant, or 24 char str, received '{type(obj)}'")
     return study_object_id + "/" + key_path
-
-
-# def s3_construct_participant_key_path(key_path: str, study_object_id: str, patient_id: str):
-# return study_object_id + "/" + patient_id + "/" + key_path
 
 
 def s3_upload(
@@ -92,7 +76,7 @@ def s3_retrieve(
     return decrypt_server(encrypted_data, smart_get_study_encryption_key(obj))
 
 
-def _do_retrieve(bucket_name, key_path, number_retries=3):
+def _do_retrieve(bucket_name: str, key_path: str, number_retries=3):
     """ Run-logic to do a data retrieval for a file in an S3 bucket."""
     assert S3_BUCKET is not Exception, "libs.s3._s3_retrieve(!!!) called inside test"
     try:
@@ -110,21 +94,88 @@ def _do_retrieve(bucket_name, key_path, number_retries=3):
         raise
 
 
-def s3_list_files(prefix, as_generator=False) -> List[str]:
-    """ Method fetches a list of filenames with prefix.
-        note: entering the empty string into this search without later calling
-        the object results in a truncated/paginated view."""
+def s3_list_files(prefix: str, as_generator=False) -> List[str]:
+    """ Lists s3 keys matching prefix. as generator returns a generator instead of a list.
+    WARNING: passing in an empty string can be dangerous. """
     assert S3_BUCKET is not Exception, "libs.s3.s3_list_files called inside test"
     return _do_list_files(S3_BUCKET, prefix, as_generator=as_generator)
 
 
 def smart_s3_list_study_files(prefix: str, obj: StrOrParticipantOrStudy):
+    """ Lists s3 keys matching prefix, autoinserting the study object id at start of key path. """
     assert S3_BUCKET is not Exception, "libs.s3.smart_s3_list_study_files called inside test"
     return s3_list_files(s3_construct_study_key_path(prefix, obj))
 
 
+# should work, not tested.
+# def smart_s3_list_chunked_files(prefix: str, obj: StrOrParticipantOrStudy):
+#     """ Lists s3 keys matching prefix, autoinserting the study object id at start of key path. """
+#     assert S3_BUCKET is not Exception, "libs.s3.smart_s3_list_study_files called inside test"
+#     return s3_list_files(f"{CHUNKS_FOLDER}/{s3_construct_study_key_path(prefix, obj)}")
+
+
+def _do_list_files(bucket_name: str, prefix: str, as_generator=False) -> List[str]:
+    paginator = conn.get_paginator('list_objects_v2')
+    assert S3_BUCKET is not Exception, "libs.s3.__s3_list_files(!!!) called inside test"
+    page_iterator: Paginator = paginator.paginate(Bucket=bucket_name, Prefix=prefix)
+    if as_generator:
+        return _do_list_files_generator(page_iterator)
+    else:
+        items = []
+        for page in page_iterator:
+            if 'Contents' in page:
+                for item in page['Contents']:
+                    items.append(item['Key'].strip("/"))
+        return items
+
+
+def _do_list_files_generator(page_iterator: Paginator):
+    for page in page_iterator:
+        if 'Contents' not in page:
+            return
+        for item in page['Contents']:
+            yield item['Key'].strip("/")
+
+
+def s3_delete(key_path: str):
+    raise Exception("NO DONT DELETE")
+
+################################################################################
+######################### Client Key Management ################################
+################################################################################
+
+
+def create_client_key_pair(patient_id: str, study_id: str):
+    """Generate key pairing, push to database, return sanitized key for client."""
+    public, private = generate_key_pairing()
+    s3_upload("keys/" + patient_id + "_private", private, study_id)
+    s3_upload("keys/" + patient_id + "_public", public, study_id)
+
+
+def get_client_public_key_string(patient_id: str, study_id: str) -> str:
+    """Grabs a user's public key string from s3."""
+    key_string = s3_retrieve("keys/" + patient_id + "_public", study_id)
+    return prepare_X509_key_for_java(key_string).decode()
+
+
+def get_client_public_key(patient_id: str, study_id: str) -> RSA.RsaKey:
+    """Grabs a user's public key file from s3."""
+    key = s3_retrieve("keys/" + patient_id +"_public", study_id)
+    return get_RSA_cipher(key)
+
+
+def get_client_private_key(patient_id: str, study_id: str) -> RSA.RsaKey:
+    """Grabs a user's private key file from s3."""
+    key = s3_retrieve("keys/" + patient_id +"_private", study_id)
+    return get_RSA_cipher(key)
+
+
+###############################################################################
+
+
 def s3_list_versions(prefix, allow_multiple_matches=False):
-    """
+    """ (Moved to bottom of file because versioning is not even enabled by default.)
+
     Page structure - each page is a dictionary with these keys:
      Name, ResponseMetadata, Versions, MaxKeys, Prefix, KeyMarker, IsTruncated, VersionIdMarker
     We only care about 'Versions', which is a list of all object versions matching that prefix.
@@ -155,58 +206,17 @@ def s3_list_versions(prefix, allow_multiple_matches=False):
             })
     return versions
 
+###############################################################################
 
-def _do_list_files(bucket_name, prefix, as_generator=False) -> List[str]:
-    paginator = conn.get_paginator('list_objects_v2')
-    assert S3_BUCKET is not Exception, "libs.s3.__s3_list_files(!!!) called inside test"
-    page_iterator = paginator.paginate(Bucket=bucket_name, Prefix=prefix)
-    if as_generator:
-        return _do_list_files_generator(page_iterator)
-    else:
-        items = []
-        for page in page_iterator:
-            if 'Contents' in page:
-                for item in page['Contents']:
-                    items.append(item['Key'].strip("/"))
-        return items
+""" Research on getting a stream into the decryption code of pycryptodome
 
+The StreamingBody StreamingBody object does not define the __len__ function, which is
+necessary for creating a buffer somewhere in the decryption code, but it is possible to monkeypatch
+it in like this:
+    import botocore.response
+    def monkeypatch_len(self):
+        return int(self._content_length)
+    botocore.response.StreamingBody.__len__ = monkeypatch_len
 
-def _do_list_files_generator(page_iterator):
-    for page in page_iterator:
-        if 'Contents' not in page:
-            return
-        for item in page['Contents']:
-            yield item['Key'].strip("/")
-
-
-def s3_delete(key_path):
-    raise Exception("NO DONT DELETE")
-
-################################################################################
-######################### Client Key Management ################################
-################################################################################
-
-
-def create_client_key_pair(patient_id, study_id):
-    """Generate key pairing, push to database, return sanitized key for client."""
-    public, private = generate_key_pairing()
-    s3_upload("keys/" + patient_id + "_private", private, study_id)
-    s3_upload("keys/" + patient_id + "_public", public, study_id)
-
-
-def get_client_public_key_string(patient_id, study_id) -> str:
-    """Grabs a user's public key string from s3."""
-    key_string = s3_retrieve("keys/" + patient_id + "_public", study_id)
-    return prepare_X509_key_for_java(key_string).decode()
-
-
-def get_client_public_key(patient_id, study_id) -> RSA.RsaKey:
-    """Grabs a user's public key file from s3."""
-    key = s3_retrieve("keys/" + patient_id +"_public", study_id)
-    return get_RSA_cipher(key)
-
-
-def get_client_private_key(patient_id, study_id) -> RSA.RsaKey:
-    """Grabs a user's private key file from s3."""
-    key = s3_retrieve("keys/" + patient_id +"_private", study_id)
-    return get_RSA_cipher(key)
+But that just results in this error from pycryptodome:
+TypeError: Object type <class 'botocore.response.StreamingBody'> cannot be passed to C code """

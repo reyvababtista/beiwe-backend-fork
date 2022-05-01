e5441b1d6aea2ae4e3ed252d0ef17e4d77c8b227
Eli Jones <biblicabeebli@gmail.com>
2022-05-01 03:06:35 -0400

    [tableau-fixes] completely untested rewrite of the tableau data endpoint

--- libs/utils/effiicient_paginator.py
+++ libs/utils/effiicient_paginator.py
@@ -1,52 +1,60 @@
 from typing import Any, Dict, List
 
+from orjson import dumps as orjson_dumps
+
 from database.common_models import UtilityModel
 
 
 class EfficientPaginator():
     """ Queries for the PKs and retrieves results using them.
-    The result is a minimal memory overhead database query.  """
+    The result is a minimal memory overhead database query. """
     
-    def __init__(self,
+    def __init__(
+        self,
         model: UtilityModel,
         page_size: int,
+        limit: int = 0,
         filter_kwargs: Dict[str, Any] = None,
         order_args: List[str] = None,
+        annotate_kwargs: Dict = None,
         values: List[str] = None,
         values_list: List[str] = None,
-        flat=True
+        flat=True,
     ):
+        if values and values_list:
+            raise Exception("only one of values or values_list may be provided")
+        
         filter_kwargs = filter_kwargs or {}
         order_args = order_args or []
+        annotate_kwargs = annotate_kwargs or {}
         
-        # setup efficient-as-possibly query for database PKs
-        # using iterator allows quick initial query, but paging may be inconsistent in time.
+        # setup efficient-as-possibly query for database PKs, annotate must be applied in order to
+        # handle possible using iterator allows quick initial query, which is universally better.
         self.page_size = page_size
-        self.pk_query = (
-                model.objects
-                .filter(**filter_kwargs)
-                .order_by(*order_args)
-                .values_list("pk", flat=True)
-                .iterator()
-            )
+        self.pk_query = model.objects.annotate(**annotate_kwargs) \
+            .filter(**filter_kwargs).order_by(*order_args).values_list("pk", flat=True)
         
-        if values and values_list:
-            raise Exception("one of values and values_list")
+        # apply limit if provided
+        if limit:
+            self.pk_query = self.pk_query[:limit]
+        
+        # and make it an iterator to avoid caching (matters for massive queries)
+        self.pk_query = self.page_size.iterator()
+        
+        # annotate before filtering in case of dependencies
+        self.unfiltered_query = model.objects
+        if annotate_kwargs:
+            self.unfiltered_query = self.unfiltered_query.annotate(**annotate_kwargs)
         
         # pass params intelligently
         if values:
-            self.real_query = model.objects.values(*values)
+            self.unfiltered_query = self.unfiltered_query.values(*values)
         elif values_list:
-            if flat and len(values_list) == 1:
-                self.real_query = model.objects.values_list(*values_list, flat=True)
-            else:
-                self.real_query = model.objects.values_list(*values_list)
-        else:
-            self.real_query = model.objects.filter()
+            self.unfiltered_query = self.unfiltered_query.values_list(
+                *values_list, flat=flat and len(values_list) == 1)
         
-        # may be unnecessary depending on database backend
-        if order_args:
-            self.real_query = self.real_query.order_by(*order_args)
+        if order_args:  # order of pks is not preserved
+            self.unfiltered_query = self.unfiltered_query.order_by(*order_args)
     
     def __iter__(self):
         """ Grab a page of PKs, the results via iteration. """
@@ -54,13 +62,13 @@ class EfficientPaginator():
         for count, pk in enumerate(self.pk_query, start=1):
             pks.append(pk)
             if count % self.page_size == 0:
-                for result in self.real_query.filter(pk__in=pks):
+                for result in self.unfiltered_query.filter(pk__in=pks):
                     yield result
                 pks = []
         
         # after iteration, any remaining pks
         if pks:
-            for result in self.real_query.filter(pk__in=pks):
+            for result in self.unfiltered_query.filter(pk__in=pks):
                 yield result
     
     def paginate(self):
@@ -70,9 +78,16 @@ class EfficientPaginator():
             pks.append(pk)
             if count % self.page_size == 0:
                 # using list(query) the iteration occurs inside cpython and is extremely quick.
-                yield list(self.real_query.filter(pk__in=pks))
+                yield list(self.unfiltered_query.filter(pk__in=pks))
                 pks = []
         
         # after iteration, any remaining pks
         if pks:
-            yield list(self.real_query.filter(pk__in=pks))
+            yield list(self.unfiltered_query.filter(pk__in=pks))
+    
+    def stream_orjson_paginate(self):
+        """ streams a page by page orjson'd bytes of json list elements """
+        yield b"["
+        for page in self.paginate():
+            yield orjson_dumps(page)[1:-1]
+        yield b"]"

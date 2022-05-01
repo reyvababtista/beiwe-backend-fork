73abf378bf44727b1fdd4d0edf0fb6d66761e3e5
Eli Jones <biblicabeebli@gmail.com>
2022-05-01 03:06:35 -0400

    [tableau-fixes] completely untested rewrite of the tableau data endpoint

--- api/tableau_api.py
+++ api/tableau_api.py
@@ -1,23 +1,22 @@
 import json
+from typing import List
 
-from django.db.models import QuerySet
-from django.http.response import HttpResponse
+from django.db.models import F
+from django.http import StreamingHttpResponse
 from django.shortcuts import render
 from django.views.decorators.http import require_GET
-from rest_framework.renderers import JSONRenderer
 
 from authentication.tableau_authentication import authenticate_tableau
 from constants.tableau_api_constants import FIELD_TYPE_MAP, SERIALIZABLE_FIELD_NAMES
 from database.tableau_api_models import SummaryStatisticDaily
 from forms.django_forms import ApiQueryForm
 from libs.internal_types import TableauRequest
-from serializers.tableau_serializers import SummaryStatisticDailySerializer
+from libs.utils.effiicient_paginator import EfficientPaginator
 
 
 FINAL_SERIALIZABLE_FIELD_NAMES = (
     f for f in SummaryStatisticDaily._meta.fields if f.name in SERIALIZABLE_FIELD_NAMES
 )
-
 
 @require_GET
 @authenticate_tableau
@@ -25,37 +24,39 @@ def get_tableau_daily(request: TableauRequest, study_object_id: str = None):
     form = ApiQueryForm(data=request.POST)
     if not form.is_valid():
         return format_errors(form.errors.get_json_data())
-    query = tableau_query_database(study_object_id=study_object_id, **form.cleaned_data)
-    serializer = SummaryStatisticDailySerializer(
-        query, fields=form.cleaned_data["fields"], many=True,
+    
+    # The don't need to specify the study_id and participant_id fields, those are provided.
+    query_fields = [f for f in form.cleaned_data["fields"] if f in SERIALIZABLE_FIELD_NAMES]
+    paginator = tableau_query_database(
+        study_object_id=study_object_id,  # the object id is validated in the login logic
+        query_fields=query_fields,
+        **form.cleaned_data,  # already cleaned and validated
     )
-    return HttpResponse(JSONRenderer().render(serializer.data))
+    return StreamingHttpResponse(paginator.stream_orjson_paginate, content_type="application/json")
 
 
 @require_GET
 def web_data_connector(request: TableauRequest, study_object_id: str):
-    # build the columns datastructure for tableau to enumerate the format of the API data
-    columns = ['[\n']
-    # study_id and participant_id are not part of the SummaryStatisticDaily model, so they
-    # aren't populated. They are also related fields that both are proxies for a unique
-    # identifier field that has a different name, so we do it manually.
-    # TODO: this could be less messy.
-    columns.append("{id: 'study_id', dataType: tableau.dataTypeEnum.string,},\n")
-    columns.append("{id: 'participant_id', dataType: tableau.dataTypeEnum.string,},\n")
+    """ Build the columns datastructure for tableau to enumerate the format of the API data. """
+    # study_id and participant_id are not part of the SummaryStatisticDaily model, so they aren't
+    # populated. They are also related fields that both are proxies for a unique identifier field
+    # that has a different name, so we do it manually.
+    columns = ['[\n',
+               "{id: 'study_id', dataType: tableau.dataTypeEnum.string,},\n",
+               "{id: 'participant_id', dataType: tableau.dataTypeEnum.string,},\n"]
+    
     for field in FINAL_SERIALIZABLE_FIELD_NAMES:
-        for (py_type, tableau_type) in FIELD_TYPE_MAP:
-            if isinstance(field, py_type):
+        for (python_type, tableau_type) in FIELD_TYPE_MAP:
+            if isinstance(field, python_type):
                 columns.append(f"{{id: '{field.name}', dataType: {tableau_type},}},\n")
-                # ex line: {id: 'participant_id', dataType: tableau.dataTypeEnum.int,},
+                # example line: {id: 'participant_id', dataType: tableau.dataTypeEnum.int,},
                 break
         else:
             # if the field is not recognized, supply it to tableau as a string type
             columns.append(f"{{id: '{field.name}', dataType: tableau.dataTypeEnum.string,}},\n")
-    columns.append('];')
-    return render(
-        request,
-        'wdc.html', context={"study_object_id": study_object_id, "cols": "".join(columns)}
-    )
+    
+    columns = "".join(columns) + '];'
+    return render(request, 'wdc.html', context=dict(study_object_id=study_object_id, cols=columns))
 
 
 def format_errors(errors: dict) -> str:
@@ -67,13 +68,13 @@ def format_errors(errors: dict) -> str:
 
 
 def tableau_query_database(
-    study_object_id, participant_ids=None, limit=None,       # basics
-    end_date=None, start_date=None,                          # time
-    order_by="date", order_direction="descending",           # sort
-    **_   # Because Whimsy is important.                     # ignore everything else
-) -> QuerySet:
-    """
-    Args:
+    study_object_id, participant_ids=None, limit=None,  # basics
+    end_date=None, start_date=None,                     # time
+    order_by="date", order_direction="descending",      # sort
+    query_fields: List[str] = None,
+    **_   # Because Whimsy is important.                # ignore everything else
+) -> EfficientPaginator:
+    """ Args:
         study_object_id (str): study in which to find data
         end_date (optional[date]): last date to include in search
         start_date (optional[date]): first date to include in search
@@ -81,20 +82,37 @@ def tableau_query_database(
         order_by (str): parameter to sort output by. Must be one in the list of fields to return
         order_direction (str): order to sort in, either "ascending" or "descending"
         participant_ids (optional[list[str]]): a list of participants to limit the search to
-
-    Returns (queryset[SummaryStatisticsDaily]):
-            the SummaryStatisticsDaily objects specified by the parameters
-    """
+    Returns EfficientPaginator of the SummaryStatisticsDaily objects specified by the parameters """
+    
+    if not query_fields:
+        raise Exception("invalid usage")
+    
+    filter_kwargs = {}
+    filter_kwargs["participant__study__object_id"] = study_object_id
+    if participant_ids:
+        filter_kwargs["participant__patient_id__in"] = participant_ids
+    if end_date:
+        filter_kwargs["date__lte"] = end_date
+    if start_date:
+        filter_kwargs["date__gte"] = start_date
+    
     if order_direction == "descending":
         order_by = "-" + order_by
-    queryset = SummaryStatisticDaily.objects.filter(participant__study__object_id=study_object_id)
-    if participant_ids:
-        queryset = queryset.filter(participant__patient_id__in=participant_ids)
-    if end_date:
-        queryset = queryset.filter(date__lte=end_date)
-    if start_date:
-        queryset = queryset.filter(date__gte=start_date)
-    queryset = queryset.order_by(order_by)
-    if limit:
-        queryset = queryset[:limit]
-    return queryset
+    
+    if not limit:
+        limit = 0
+    
+    annotate_kwargs = {}
+    if "participant_id" in query_fields:
+        annotate_kwargs["participant_id"] = F("participant__patient_id")
+    if "study_id" in query_fields:
+        annotate_kwargs["study_id"] = F("participant__study__object_id")
+    
+    return EfficientPaginator(
+        SummaryStatisticDaily,
+        10000,  # page size
+        filter_kwargs=filter_kwargs,
+        order_args=(order_by,),
+        limit=limit,
+        annotate_kwargs=annotate_kwargs
+    )

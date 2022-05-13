d9052716745cd6002d68416a4aab48185e4e5ff4
Eli Jones <biblicabeebli@gmail.com>
2022-05-13 14:58:56 -0400

    [backup-access-logs] shift backup log to run using new script runner!

--- scripts/upload_logs.py
+++ scripts/upload_logs.py
@@ -3,22 +3,20 @@ from subprocess import check_output
 from django.utils import timezone
 
 from libs.s3 import s3_upload_plaintext
-from libs.sentry import make_error_sentry, SentryTypes
 
+"""
+this script runs periodically. The default system logrotate periodicity for the amazon ubuntu 18.04
+image is weekly, as long as this script is run more frequently than that all log data in the auth
+log will be uploaded.
+"""
 
-# this script runs periodically. The default system logrotate periodicity for the amazon
-# ubuntu 18.04 image is weekly, as long as this script is run more frequently than that 
-# all log data in the auth log will be uploaded.
+with open("/var/log/auth.log") as f:
+    auth_log = f.read()
 
+now = timezone.now().isoformat()
 
-with make_error_sentry(SentryTypes.data_processing):
-    with open("/var/log/auth.log") as f:
-        auth_log = f.read()
+# should be something like ip-172-31-67-107
+hostname = check_output("hostname").strip().decode()
 
-    now = timezone.now().isoformat()
-    
-    # should be something like ip-172-31-67-107
-    hostname = check_output("hostname").strip().decode()
-    
-    # file name should sort by hostname then date
-    s3_upload_plaintext(f"LOGS/auth_log/{hostname}-{now}.log", auth_log)
+# file name should sort by hostname then date
+s3_upload_plaintext(f"LOGS/auth_log/{hostname}-{now}.log", auth_log)

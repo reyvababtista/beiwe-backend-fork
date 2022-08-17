4705e551950e5593cc577e046e4b8e9ddc4b0e98
Eli Jones <biblicabeebli@gmail.com>
2022-08-16 20:58:50 -0400

    [staged-updates] performance fix (unnegessacy database calls) in file processing

--- libs/utils/dev_utils.py
+++ libs/utils/dev_utils.py
@@ -22,7 +22,7 @@ class TxtClr:
     YELLOW = "\x1b[33m"
     GREEN = "\x1b[32m"
     CYAN = "\x1b[36m"
-
+    
     @classmethod
     def brightness_swap(cls):
         if cls.RED != "\x1b[31m":
@@ -44,33 +44,33 @@ def disambiguate_participant_survey(func):
     @functools.wraps(func)
     def _disambiguate_participant_survey(*args, **kwargs):
         args = list(args)  # not initially mutable
-
+        
         participant = args[0]  # The first parameter is positional
         survey = args[1] if len(args) >= 2 else None
-
+        
         msg = "pass in a survey object, a survey's object_id key, a participant, or a participnt's patient_id"
         assert participant is None or isinstance(participant, (Survey, Participant, str)), msg
         assert survey is None or isinstance(survey, (Survey, str)), msg
-
+        
         # case: (participant: None, survey: something).  we actually handle that in reverse already!
         if participant is None and survey is not None:
             participant, survey = survey, participant
-
+        
         # allows passing in just a survey - if no survey and participant is a survey
         if not survey and isinstance(participant, Survey):
             participant, survey = survey, participant
-
+        
         # if only a participant but its an object_id
         if isinstance(participant, str) and len(participant) == 24:
             participant, survey = survey, participant
-
+        
         # string to participant
         if isinstance(participant, str):
             try:
                 participant = Participant.objects.get(patient_id=participant)
             except Participant.DoesNotExist:
                 raise TypeError(f"no matching participant for '{participant}'")
-
+        
         # string to survey
         if isinstance(survey, str):
             if len(survey) == 24:
@@ -80,7 +80,7 @@ def disambiguate_participant_survey(func):
                     pass
             else:
                 raise TypeError(f"'{survey}' was a string, but it had the wrong length...")
-
+        
         # reassign and/or add
         args[0] = participant
         # if we swapped survey or if there was originally a parameter we treated as a survey
@@ -88,9 +88,9 @@ def disambiguate_participant_survey(func):
             args[1] = survey
         elif survey:
             args.append(survey)
-
+        
         return func(*args, **kwargs)
-
+    
     return _disambiguate_participant_survey
 
 
@@ -103,17 +103,17 @@ class GlobalTimeTracker:
     if there are any events.  The __del__ function is an instance method,
     so by initializing lazily we skip it entirely.
     """
-
+    
     function_pointers = defaultdict(list)
     global_time_tracker = None
-
+    
     @classmethod
     def add_event(
             cls, function: FunctionType or MethodType, duration: float, exception: Exception = None
     ):
         # initialize and point to new function on the first call
         cls.global_time_tracker = GlobalTimeTracker()
-
+        
         #
         # This is the REAL add_event function
         #
@@ -128,15 +128,15 @@ class GlobalTimeTracker:
                 cls.function_pointers[function. __name__, function].append(duration)
             else:
                 cls.function_pointers[function.__name__, function, str(exception)].append(duration)
-
+        
         # repoint, call.
         cls.add_event = _add_event
         cls.add_event(function=function, duration=duration, exception=exception)
-
+    
     @classmethod
     def print_summary(cls):
         """ Prints a summary of runtime statistics."""
-
+        
         for components, times in cls.function_pointers.items():
             name = components[0]
             pointer = components[1]
@@ -144,7 +144,7 @@ class GlobalTimeTracker:
             # these as variable names is easier
             name_and_exception = f"{name} {str(exception)}"
             final_name = name if not exception else name_and_exception
-
+            
             print(
                 "\n"
                 f"function: {final_name}",
@@ -163,11 +163,11 @@ class GlobalTimeTracker:
                 "\n"
                 f"stdev: {'xxx' if len(times) == 1 else stdev(times)}"
             )
-
+    
     def __del__(self, *args, **kwargs):
         """ On deallocation, print a bunch of statistics. """
         self.print_summary()
-
+    
     @staticmethod
     def track_function(some_function):
         """ wraps a function with a timer that records to a GlobalTimeTracker"""
@@ -205,12 +205,12 @@ def print_entry_and_return_types(some_function):
     @functools.wraps(some_function)
     def wrapper(*args, **kwargs):
         name = getframeinfo(stack()[1][0]).filename.strip(PROJECT_PATH) + ": " + some_function.__name__
-
+        
         # args and kwargs COULD mutate
         args_dict = {i: type(v) for i, v in enumerate(args)}
         kwargs_dict = {k: type(v) for k, v in kwargs.items()}
         # don't print multiple times...
-
+        
         # place before adding to processed
         if name in already_processed:
             try:
@@ -223,36 +223,36 @@ def print_entry_and_return_types(some_function):
                     print(f"kwargs in {name} (IT ERRORED!):")
                     pprint(kwargs_dict)
                 raise
-
+        
         already_processed.add(name)
-
+        
         rets = some_function(*args, **kwargs)
-
+        
         if args_dict:
             print(f"args in {name}:")
             pprint(args_dict)
-
+        
         if kwargs_dict:
             print(f"kwargs in {name}:")
             pprint(kwargs_dict)
-
+        
         if isinstance(rets, tuple):
             types = ", ".join(str(type(t)) for t in rets)
             print(f"return types - {name} -> ({types})")
         else:
             print(f"return type - {name} -> {type(rets)}")
         return rets
-
+    
     return wrapper
 
 
 class timer_class():
     """ This is a simple class that is at the heart of the p() function declared below.
         This class consists of a datetime timer and a single function to access and advance it. """
-
+    
     def __init__(self):
         self.timestamp = 0
-
+    
     def set_timer(self, timestamp):
         self.timestamp = timestamp
 
@@ -261,7 +261,19 @@ class timer_class():
 timers = defaultdict(timer_class)
 
 
-def p(timer_label=0, outer_caller=False):
+def pwrap(a_function):
+    @functools.wraps(a_function)
+    def wrapper(*args, **kwargs):
+        p(a_function.__name__, quiet=True)
+        ret = a_function(*args, **kwargs)
+        p(a_function.__name__)
+        return ret
+    
+    return wrapper
+
+
+
+def p(timer_label=0, outer_caller=False, quiet=False):
     """ Handy little function that prints the file name line number it was called on and the
         amount of time since the function was last called.
         If you provide a label (anything with a string representation) that will be printed
@@ -302,10 +314,10 @@ def p(timer_label=0, outer_caller=False):
         caller = getframeinfo(stack()[2][0])
     else:
         caller = getframeinfo(stack()[1][0])
-
+    
     print("%s:%.f -- %s --" % (relpath(caller.filename), caller.lineno, timer_label), end=" ")
     # the first call to get_timer results in a zero elapsed time, so we can skip it.
-    if timer_object.timestamp == 0:
+    if timer_object.timestamp == 0 or quiet:
         print("timer start...")
     else:
         print('%.10f' % (timestamp - timer_object.timestamp))

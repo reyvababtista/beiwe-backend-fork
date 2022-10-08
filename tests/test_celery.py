feee07200c36f392fab05b8ef9c42f6a1c25d8bb
Eli Jones <biblicabeebli@gmail.com>
2022-10-07 23:57:58 -0400

    [staged-updates] more trivial tests

--- tests/test_celery.py
+++ tests/test_celery.py
@@ -34,7 +34,7 @@ class TestGetSurveysAndSchedules(TestCelery):
     
     # just a placeholder for future work, send_notification not actually called in this test
     @patch('services.celery_push_notifications.send_notification')
-    def test_absolute_basics(self, send_notification: MagicMock):
+    def test_absolute_success(self, send_notification: MagicMock):
         send_notification.return_value = None
         
         self.populate_default_fcm_token
@@ -46,7 +46,17 @@ class TestGetSurveysAndSchedules(TestCelery):
         self.assertEqual(schedules, self.DEFAULT_SCHEDULES_DICT(schedule))
         self.assertEqual(patient_ids, self.DEFAULT_PATIENT_IDS_DICT)
     
-    def test_relative_basics(self):
+    def test_absolute_fail(self):
+        self.populate_default_fcm_token
+        future = timezone.now() + timedelta(days=5)
+        # an absolute survey 5 days in the future
+        self.generate_easy_absolute_schedule_event_with_schedule(future)
+        surveys, schedules, patient_ids = get_surveys_and_schedules(timezone.now())
+        self.assertEqual(surveys, {})
+        self.assertEqual(schedules, {})
+        self.assertEqual(patient_ids, {})
+    
+    def test_relative_success(self):
         self.populate_default_fcm_token
         # a relative survey 5 days in the past
         schedule = self.generate_easy_relative_schedule_event_with_schedule(timedelta(days=-5))
@@ -55,8 +65,17 @@ class TestGetSurveysAndSchedules(TestCelery):
         self.assertEqual(schedules, self.DEFAULT_SCHEDULES_DICT(schedule))
         self.assertEqual(patient_ids, self.DEFAULT_PATIENT_IDS_DICT)
     
+    def test_relative_failure(self):
+        self.populate_default_fcm_token
+        # a relative survey 5 days in the past
+        self.generate_easy_relative_schedule_event_with_schedule(timedelta(days=5))
+        surveys, schedules, patient_ids = get_surveys_and_schedules(timezone.now())
+        self.assertEqual(surveys, {})
+        self.assertEqual(schedules, {})
+        self.assertEqual(patient_ids, {})
+    
     @time_machine.travel(OCT_6_NOON_2022)
-    def test_weekly_basics(self):
+    def test_weekly_success(self):
         self.populate_default_fcm_token
         # a weekly survey, on a friday, sunday is the zero-index; I hate it more than you.
         schedule, count_created = self.generate_a_real_weekly_schedule_event_with_schedule(5)
@@ -66,3 +85,14 @@ class TestGetSurveysAndSchedules(TestCelery):
             self.assertEqual(surveys, self.DEFAULT_SURVEYS_DICT)
             self.assertEqual(schedules, self.DEFAULT_SCHEDULES_DICT(schedule))
             self.assertEqual(patient_ids, self.DEFAULT_PATIENT_IDS_DICT)
+    
+    @time_machine.travel(OCT_6_NOON_2022)
+    def test_weekly_in_future_fails(self):
+        self.populate_default_fcm_token
+        # a weekly survey, on a friday, sunday is the zero-index; I hate it more than you.
+        schedule, count_created = self.generate_a_real_weekly_schedule_event_with_schedule(5)
+        self.assertEqual(count_created, 1)
+        surveys, schedules, patient_ids = get_surveys_and_schedules(timezone.now())
+        self.assertEqual(surveys, {})
+        self.assertEqual(schedules, {})
+        self.assertEqual(patient_ids, {})

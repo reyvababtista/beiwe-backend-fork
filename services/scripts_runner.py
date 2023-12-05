233c6bcd09b52c309e02c842835340bb4ce23a0c
Eli Jones <biblicabeebli@gmail.com>
2023-12-04 21:57:24 -0500

    [purge-bad-data] updates due to code churn.

--- services/scripts_runner.py
+++ services/scripts_runner.py
@@ -10,6 +10,11 @@ from libs.celery_control import safe_apply_async, scripts_celery_app
 from libs.sentry import make_error_sentry, SentryTypes
 
 
+SIX_MINUTELY = "six_minutely"
+HOURLY = "hourly"
+DAILY = "daily"
+
+
 class ImportRepeater():
     @classmethod
     def ensure_run(cls, module: Module):
@@ -23,13 +28,15 @@ class ImportRepeater():
 
 
 def queue_script(a_celery_task: Callable, expiry: str):
-    """ forces enqueueing with an expiry. """
-    if expiry not in ("daily", "hourly"):
-        raise ValueError("Expiry must one of daily or hourly.")
+    """ Forces enqueueing with an expiry. """
+    if expiry not in (SIX_MINUTELY, HOURLY, DAILY):
+        raise ValueError("Expiry must be one of the constants in this file.")
     
-    if expiry == "daily":
+    if expiry == SIX_MINUTELY:
+        expiry = (timezone.now() + timedelta(minutes=6)).replace(second=0, microsecond=0)
+    if expiry == DAILY:
         expiry = (timezone.now() + timedelta(hours=24)).replace(second=0, microsecond=0)
-    if expiry == "hourly":
+    if expiry == HOURLY:
         expiry = (timezone.now() + timedelta(hours=1)).replace(second=0, microsecond=0)
     
     print(f"Queueing script '{a_celery_task.__name__}', expires at {expiry}")
@@ -48,7 +55,7 @@ def queue_script(a_celery_task: Callable, expiry: str):
 def create_task_ios_no_decryption_key_task():
     with make_error_sentry(sentry_type=SentryTypes.data_processing):
         print("Queueing ios bad decryption keys script.")
-        queue_script(celery_process_ios_no_decryption_key, "hourly")
+        queue_script(celery_process_ios_no_decryption_key, HOURLY)
 
 
 @scripts_celery_app.task(queue=SCRIPTS_QUEUE)
@@ -63,7 +70,7 @@ def celery_process_ios_no_decryption_key():
 
 def create_task_upload_logs():
     with make_error_sentry(sentry_type=SentryTypes.data_processing):
-        queue_script(celery_upload_logs, "daily")
+        queue_script(celery_upload_logs, DAILY)
 
 
 # run via celery as long as tasks exist
@@ -80,7 +87,7 @@ def celery_upload_logs():
 def create_task_participant_data_deletion():
     with make_error_sentry(sentry_type=SentryTypes.data_processing):
         print("Queueing participant data deletion task.")
-        safe_apply_async(celery_participant_data_deletion)
+        queue_script(celery_participant_data_deletion, HOURLY)
 
 
 #run via celery as long as tasks exist
@@ -96,7 +103,7 @@ def celery_participant_data_deletion():
 def create_task_update_celery_version():
     with make_error_sentry(sentry_type=SentryTypes.data_processing):
         print("Queueing update celery version task.")
-        safe_apply_async(celery_update_forest_version)
+        queue_script(celery_update_forest_version, SIX_MINUTELY)
 
 
 @scripts_celery_app.task(queue=SCRIPTS_QUEUE)
@@ -107,10 +114,10 @@ def celery_update_forest_version():
         ImportRepeater.ensure_run(update_forest_version)
 
 
-# purge old data
+## purge data from invalid timestamps
 def create_task_purge_invalid_time_data():
     with make_error_sentry(sentry_type=SentryTypes.data_processing):
-        queue_script(celery_purge_invalid_time_data, "daily")
+        queue_script(celery_purge_invalid_time_data, DAILY)
 
 
 # run via celery as long as tasks exist

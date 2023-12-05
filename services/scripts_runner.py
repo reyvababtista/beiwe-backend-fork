8ab79430e6d48b1faaec0b472d9d97fcc84b3375
Eli Jones <biblicabeebli@gmail.com>
2023-12-04 23:18:18 -0500

    [purge-bad-data] moooore cleanup of the script runner.

--- services/scripts_runner.py
+++ services/scripts_runner.py
@@ -10,19 +10,26 @@ from libs.celery_control import safe_apply_async, scripts_celery_app
 from libs.sentry import make_error_sentry, SentryTypes
 
 
-SIX_MINUTELY = "six_minutely"
+SIX_MINUTELY = "six_minutely"  # SOME DAY we will have better than 6 minute minute celery tasks.
 HOURLY = "hourly"
 DAILY = "daily"
 
 
-class ImportRepeater():
+SCRIPT_ERROR_SENTRY = make_error_sentry(sentry_type=SentryTypes.script_runner)  # we only need one.
+
+class ImportRepeater:
+    """ Class used to ensure that a script is run more than once, the class itself serves as a spot
+    to store the record of the script having been run. """
+    
     @classmethod
     def ensure_run(cls, module: Module):
+        """ Ensures that a script, which must be run by importing it because _it's a script_ is run
+        more than just the first time it is imported. """
         if hasattr(cls, module.__name__):
-            # module was run once
+            # case: module was run once before, reload (rerun) it.
             importlib.reload(module)
         else:
-            # this is first run, which means module was just imported (run)
+            # case: this is first run (no record of a previous run), so it was just imported (run).
             setattr(cls, module.__name__, True)
         print(f"Ran script '{module.__name__}'")
 
@@ -33,11 +40,12 @@ def queue_script(a_celery_task: Callable, expiry: str):
         raise ValueError("Expiry must be one of the constants in this file.")
     
     if expiry == SIX_MINUTELY:
-        expiry = (timezone.now() + timedelta(minutes=6)).replace(second=0, microsecond=0)
+        expiry = timezone.now() + timedelta(minutes=6)
     if expiry == DAILY:
-        expiry = (timezone.now() + timedelta(hours=24)).replace(second=0, microsecond=0)
+        expiry = timezone.now() + timedelta(hours=24)
     if expiry == HOURLY:
-        expiry = (timezone.now() + timedelta(hours=1)).replace(second=0, microsecond=0)
+        expiry = timezone.now() + timedelta(hours=1)
+    expiry = expiry.replace(second=0, microsecond=0)  # clear out seconds and microseconds
     
     print(f"Queueing script '{a_celery_task.__name__}', expires at {expiry}")
     safe_apply_async(
@@ -46,84 +54,95 @@ def queue_script(a_celery_task: Callable, expiry: str):
         expires=expiry,
         task_track_started=True,
         task_publish_retry=False,
-        retry=False
+        retry=False,
     )
 
 
-## ios undecryptable files fix
-
-def create_task_ios_no_decryption_key_task():
-    with make_error_sentry(sentry_type=SentryTypes.data_processing):
-        print("Queueing ios bad decryption keys script.")
-        queue_script(celery_process_ios_no_decryption_key, HOURLY)
+####################################### Six Minutely ###############################################
 
 
-@scripts_celery_app.task(queue=SCRIPTS_QUEUE)
-def celery_process_ios_no_decryption_key():
-    with make_error_sentry(sentry_type=SentryTypes.data_processing):
-        print("running script process_ios_no_decryption_key")
-        from scripts import process_ios_no_decryption_key
-        ImportRepeater.ensure_run(process_ios_no_decryption_key)
-
-
-## Log uploads
-
-def create_task_upload_logs():
-    with make_error_sentry(sentry_type=SentryTypes.data_processing):
-        queue_script(celery_upload_logs, DAILY)
-
-
-# run via celery as long as tasks exist
-@scripts_celery_app.task(queue=SCRIPTS_QUEUE)
-def celery_upload_logs():
-    with make_error_sentry(sentry_type=SentryTypes.data_processing):
-        print("running script upload_logs.")
-        from scripts import upload_logs
-        ImportRepeater.ensure_run(upload_logs)
-
-
-## Participant data deletion
-
-def create_task_participant_data_deletion():
-    with make_error_sentry(sentry_type=SentryTypes.data_processing):
-        print("Queueing participant data deletion task.")
-        queue_script(celery_participant_data_deletion, HOURLY)
-
-
-#run via celery as long as tasks exist
-@scripts_celery_app.task(queue=SCRIPTS_QUEUE)
-def celery_participant_data_deletion():
-    with make_error_sentry(sentry_type=SentryTypes.data_processing):
-        print("running script participant_data_deletion.")
-        from scripts import purge_participant_data
-        ImportRepeater.ensure_run(purge_participant_data)
-
-
-# check the forest version in the update_forest_version script
+#
+## Check the forest version in the update_forest_version script
+#
 def create_task_update_celery_version():
-    with make_error_sentry(sentry_type=SentryTypes.data_processing):
+    with SCRIPT_ERROR_SENTRY:
         print("Queueing update celery version task.")
         queue_script(celery_update_forest_version, SIX_MINUTELY)
 
 
 @scripts_celery_app.task(queue=SCRIPTS_QUEUE)
 def celery_update_forest_version():
-    with make_error_sentry(sentry_type=SentryTypes.data_processing):
+    with SCRIPT_ERROR_SENTRY:
         print("running script update_forest_version.")
         from scripts import update_forest_version
         ImportRepeater.ensure_run(update_forest_version)
 
 
-## purge data from invalid timestamps
+######################################### Hourly ###################################################
+
+
+#
+## Ios undecryptable files fix
+#
+def create_task_ios_no_decryption_key_task():
+    with SCRIPT_ERROR_SENTRY:
+        print("Queueing ios bad decryption keys script.")
+        queue_script(celery_process_ios_no_decryption_key, HOURLY)
+
+
+@scripts_celery_app.task(queue=SCRIPTS_QUEUE)
+def celery_process_ios_no_decryption_key():
+    with SCRIPT_ERROR_SENTRY:
+        print("running script process_ios_no_decryption_key")
+        from scripts import process_ios_no_decryption_key
+        ImportRepeater.ensure_run(process_ios_no_decryption_key)
+
+
+#
+## Participant data deletion
+#
+def create_task_participant_data_deletion():
+    with SCRIPT_ERROR_SENTRY:
+        print("Queueing participant data deletion task.")
+        queue_script(celery_participant_data_deletion, HOURLY)
+
+
+@scripts_celery_app.task(queue=SCRIPTS_QUEUE)
+def celery_participant_data_deletion():
+    with SCRIPT_ERROR_SENTRY:
+        print("running script participant_data_deletion.")
+        from scripts import purge_participant_data
+        ImportRepeater.ensure_run(purge_participant_data)
+
+
+######################################### Daily ####################################################
+
+#
+## Upload the ssh auth log to S3 - this is a very basic security/audit measure, so we just do it.
+#
+def create_task_upload_logs():
+    with SCRIPT_ERROR_SENTRY:
+        queue_script(celery_upload_logs, DAILY)
+
+
+@scripts_celery_app.task(queue=SCRIPTS_QUEUE)
+def celery_upload_logs():
+    with SCRIPT_ERROR_SENTRY:
+        print("running script upload_logs.")
+        from scripts import upload_logs
+        ImportRepeater.ensure_run(upload_logs)
+
+#
+## Purge all data that is from impossible timestamps - we test for this now, but have still seen it.
+#
 def create_task_purge_invalid_time_data():
-    with make_error_sentry(sentry_type=SentryTypes.data_processing):
+    with SCRIPT_ERROR_SENTRY:
         queue_script(celery_purge_invalid_time_data, DAILY)
 
 
-# run via celery as long as tasks exist
 @scripts_celery_app.task(queue=SCRIPTS_QUEUE)
 def celery_purge_invalid_time_data():
-    with make_error_sentry(sentry_type=SentryTypes.data_processing):
+    with SCRIPT_ERROR_SENTRY:
         print("running script upload_logs.")
         from scripts import purge_1970_chunks
         ImportRepeater.ensure_run(purge_1970_chunks)

abe241737e5a12cba8b680b48df5a410e255bf90
Eli Jones <biblicabeebli@gmail.com>
2024-01-16 14:34:02 -0500

    [push-notification-heartbeat] updates cron target to run the heartbeat task, other formatting.

--- services/cron.py
+++ services/cron.py
@@ -1,9 +1,10 @@
+# trunk-ignore-all(isort,black,ruff/E402)
 # add the root of the project into the path to allow cd-ing into this folder and running the script.
 from os.path import abspath
 from sys import path
 
 
-path.insert(0, abspath(__file__).rsplit('/', 2)[0])
+path.insert(0, abspath(__file__).rsplit('/', 2)[0])  # todo: it would be nice to remove this...
 
 # start actual cron-related code here
 from sys import argv
@@ -12,7 +13,8 @@ from cronutils import run_tasks
 
 from services.celery_data_processing import create_file_processing_tasks
 from services.celery_forest import create_forest_celery_tasks
-from services.celery_push_notifications import create_survey_push_notification_tasks
+from services.celery_push_notifications import (create_heartbeat_tasks,
+    create_survey_push_notification_tasks)
 from services.scripts_runner import (create_task_ios_no_decryption_key_task,
     create_task_participant_data_deletion, create_task_purge_invalid_time_data,
     create_task_update_celery_version, create_task_upload_logs)
@@ -29,13 +31,20 @@ VALID_ARGS = [FIVE_MINUTES, HOURLY, FOUR_HOURLY, DAILY, WEEKLY, MONTHLY]
 TASKS = {
     FIVE_MINUTES:
         [
-            create_file_processing_tasks, create_survey_push_notification_tasks,
-            create_forest_celery_tasks, create_task_participant_data_deletion,
-            create_task_update_celery_version
+            # uploaded data processing:
+            create_file_processing_tasks,
+            # notifications:
+            create_survey_push_notification_tasks,
+            create_heartbeat_tasks,
+            # forest:
+            create_forest_celery_tasks,
+            # scripts:
+            create_task_participant_data_deletion,
+            create_task_update_celery_version,
         ],
-    HOURLY: [create_task_ios_no_decryption_key_task],
+    HOURLY: [create_task_ios_no_decryption_key_task],  # scripts
     FOUR_HOURLY: [],
-    DAILY: [create_task_upload_logs, create_task_purge_invalid_time_data],
+    DAILY: [create_task_upload_logs, create_task_purge_invalid_time_data],  # scripts
     WEEKLY: [],
     MONTHLY: [],
 }

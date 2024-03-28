e203ec0a2aaa019199d573436e2b1a015081c0cc
Eli Jones <biblicabeebli@gmail.com>
2024-03-28 18:11:57 -0400

    [data_processing_bug] fixes upload, removes code no longer required due to dropping multithreading.

--- libs/file_processing/batched_network_operations.py
+++ libs/file_processing/batched_network_operations.py
@@ -1,8 +1,5 @@
-import sys
-import traceback
 from typing import Tuple
 
-from cronutils import ErrorHandler
 from django.utils import timezone
 
 from constants.data_processing_constants import CHUNK_EXISTS_CASE
@@ -16,46 +13,33 @@ from libs.security import chunk_hash
 # GLOBAL_TIMESTAMP = datetime.now().isoformat()
 
 
-def batch_upload(upload: Tuple[ChunkRegistry or dict, str, bytes, str], error_handler: ErrorHandler):
+def batch_upload(upload: Tuple[ChunkRegistry or dict, str, bytes, str]):
     """ Used for mapping an s3_upload function.  the tuple is unpacked, can only have one parameter. """
+    chunk, chunk_path, new_contents, study_object_id = upload
+    del upload
+    # there is an external reference to the original new_contents object so there's no way to 
+    # free it up without a refactor.
+    new_contents = decompress(new_contents)
     
-    ret = {'exception': None, 'traceback': None}
-    with error_handler:
-        try:
-            chunk, chunk_path, new_contents, study_object_id = upload
-            del upload
-            new_contents = decompress(new_contents)
-            
-            if "b'" in chunk_path:
-                raise Exception(chunk_path)
-            
-            # for use with test script to avoid network uploads
-            # with open("processing_tests/" + GLOBAL_TIMESTAMP, 'ba') as f:
-            #     f.write(b"\n\n")
-            #     f.write(new_contents)
-            #     return ret
-            
-            s3_upload(chunk_path, new_contents, study_object_id, raw_path=True)
-            
-            # if the chunk object is a chunk registry then we are updating an old one,
-            # otherwise we are creating a new one.
-            if chunk == CHUNK_EXISTS_CASE:
-                # If the contents are being appended to an existing ChunkRegistry object
-                ChunkRegistry.objects.filter(chunk_path=chunk_path).update(
-                    file_size=len(new_contents),
-                    chunk_hash=chunk_hash(new_contents).decode(),
-                    last_updated=timezone.now()
-                )
-            else:
-                ChunkRegistry.register_chunked_data(**chunk, file_contents=new_contents)
-        
-        # it broke. print stacktrace for debugging
-        except Exception as e:
-            traceback.print_exc()
-            ret['traceback'] = sys.exc_info()
-            ret['exception'] = e
-            
-            # using an error sentry we can easily report a real error with a real stack trace! :D
-            raise
+    if "b'" in chunk_path:
+        raise Exception(chunk_path)
     
-    return ret
+    # for use with test script to avoid network uploads
+    # with open("processing_tests/" + GLOBAL_TIMESTAMP, 'ba') as f:
+    #     f.write(b"\n\n")
+    #     f.write(new_contents)
+    #     return ret
+    # print("uploading:", chunk_path)
+    s3_upload(chunk_path, new_contents, study_object_id, raw_path=True)
+    
+    # if the chunk object is a chunk registry then we are updating an old one,
+    # otherwise we are creating a new one.
+    if chunk == CHUNK_EXISTS_CASE:
+        # If the contents are being appended to an existing ChunkRegistry object
+        ChunkRegistry.objects.filter(chunk_path=chunk_path).update(
+            file_size=len(new_contents),
+            chunk_hash=chunk_hash(new_contents).decode(),
+            last_updated=timezone.now()
+        )
+    else:
+        ChunkRegistry.register_chunked_data(**chunk, file_contents=new_contents)

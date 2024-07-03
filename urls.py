d33e3dbced3e6fc59fe534915685e3a8c1e06b70
Eli Jones <biblicabeebli@gmail.com>
2024-07-03 14:26:22 -0400

    [endpoint-refactors] moves study security page

--- urls.py
+++ urls.py
@@ -109,7 +109,7 @@ path("reset_researcher_mfa/<int:researcher_id>", system_admin_pages.reset_resear
 path("create_study", study_endpoints.create_study)
 path("toggle_study_forest_enabled/<int:study_id>", system_admin_pages.toggle_study_forest_enabled)
 path("hide_study/<int:study_id>", study_endpoints.hide_study)
-path("edit_study_security/<int:study_id>", system_admin_pages.study_security_page, login_redirect=SAFE)
+path("edit_study_security/<int:study_id>", study_endpoints.study_security_page, login_redirect=SAFE)
 path("change_study_security_settings/<int:study_id>", system_admin_pages.change_study_security_settings)
 path("device_settings/<int:study_id>", system_admin_pages.device_settings, login_redirect=SAFE)
 path("update_end_date/<int:study_id>", study_endpoints.update_end_date)

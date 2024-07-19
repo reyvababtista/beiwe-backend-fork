2af2eedf2d6bdc0948d8100962f9521a0a7a4ebe
Eli Jones <biblicabeebli@gmail.com>
2024-07-19 02:50:37 -0400

    [endpoint-refactors] typing annotations, renames

--- libs/http_utils.py
+++ libs/http_utils.py
@@ -12,24 +12,24 @@ from libs.internal_types import ParticipantRequest
 
 
 def easy_url(url: str, *args, **kwargs) -> str:
-    """ The django reverse function, but args and kwargs are passed thnough to the args and kwargs
+    """ The django reverse function, but args and kwargs are passed through to the args and kwargs
     variables.  (Imported in the jinja templates.) """
     return reverse(url, args=args, kwargs=kwargs)
 
 
-def astimezone_with_tz(dt: datetime, timezone: Union[tzinfo, str]):
+def astimezone_with_tz(dt: datetime, timezone: Union[tzinfo, str]) -> str:
     """ context processor function for converting and displaying a time with a timezone """
     if isinstance(timezone, str):
         timezone = tz.gettz(timezone)
     return dt.astimezone(timezone).strftime(API_TIME_FORMAT_WITH_TZ)
 
 
-def time_with_tz(dt: datetime):
+def time_with_tz(dt: datetime) -> str:
     """ context processor function for displaying a time with a timezone """
     return dt.strftime(API_TIME_FORMAT_WITH_TZ)
 
 
-def nice_iso_time_format(dt: datetime, timezone: Union[tzinfo, str]):
+def nice_iso_time_format(dt: datetime, timezone: Union[tzinfo, str]) -> str:
     """ output looks like Tuesday 2024-8-25, 4:31 PM """
     if dt is None:
         return ""
@@ -39,7 +39,7 @@ def nice_iso_time_format(dt: datetime, timezone: Union[tzinfo, str]):
     return final_dt.strftime('%A %Y-%m-%d, %-I:%M %p')
 
 
-def really_nice_time_format_with_tz(dt: datetime, timezone: Union[tzinfo, str]):
+def really_nice_time_format_with_tz(dt: datetime, timezone: Union[tzinfo, str]) -> str:
     """ output looks like Tuesday Aug 25, 2020, 4:31 PM (EST) """
     if dt is None:
         return ""
@@ -50,7 +50,7 @@ def really_nice_time_format_with_tz(dt: datetime, timezone: Union[tzinfo, str]):
     return final_dt.strftime('%A %b %-d, %Y, %-I:%M %p') + " (" + timezone.tzname(final_dt) + ")"
 
 
-def checkbox_to_boolean(list_checkbox_params: List[str], dict_all_params: Dict):
+def list_of_checkbox_strings_to_booleans(list_checkbox_params: List[str], dict_all_params: Dict) -> None:
     """ Takes a list of strings that are to be processed as checkboxes on a post parameter,
     (checkboxes supply some arbitrary value in a post if they are checked, and no value at all if
     they are not checked.), and a dict of parameters and their values to update.
@@ -62,17 +62,17 @@ def checkbox_to_boolean(list_checkbox_params: List[str], dict_all_params: Dict):
             dict_all_params[param] = True
 
 
-def string_to_int(list_int_params: List[str], dict_all_params: Dict):
+def list_of_intlike_strings_to_int(list_int_params: List[str], dict_all_params: Dict) -> None:
     for key in list_int_params:
         dict_all_params[key] = int(dict_all_params[key])
 
 
-def determine_os_api(some_function: Callable):
+def determine_os_api(some_function: Callable) -> Callable:
     """ Add this as a decorator to a url function, under (after) the wsgi route
     decorator.  It detects if the url ends in /ios.
     This decorator provides to the function with the new variable "OS_API", which can
     then be compared against the IOS_API and ANDROID_API variables in constants.
-
+    
     To handle any issues that arise from an undeclared keyword argument, throw
     'OS_API=""' into your url function declaration. """
     @functools.wraps(some_function)
